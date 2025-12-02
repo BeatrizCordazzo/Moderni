f723374f040e5ffd4a9885153d981cee2b0a2485
Bea <cordazzobea@gmail.com>
2025-12-02 21:45:51 +0100

    refactor: connection

--- src/app/datos.ts
+++ src/app/datos.ts
@@ -149,7 +149,7 @@ export interface FavoritePayload {
   providedIn: 'root',
 })
 export class Datos {
-  url = 'https://moderni.infinityfreeapp.com/moderni/';
+  url = 'http://localhost/moderni/';
   // Emit when a product is updated so other components can refresh their local lists
   private productUpdated = new Subject<any>();
   productUpdated$ = this.productUpdated.asObservable();
